0900d406cbc2cb549c1be048ca6342f93217e7da
RICHARD-Quentin <richard.quentin88@gmail.com>
2020-05-09 17:45:56 +0200

    test2

---
+++ HDV_Online_FrontV2/pages/Accueil.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon:'mdi-home', text: 'Tableau de bord', route:'/Accueil' },
+        { icon: 'mdi-cart-arrow-down', text: 'Commandes', route:'/commandes' },
+        { icon: 'mdi-account', text: 'Clients', route:'/clients' },
+        { icon: 'mdi-folder-search-outline', text: 'Produits', route:'/produits' },
+      ],
+    }),
+  }
+</script>
---
+++ HDV_Online_FrontV2/pages/produits.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "produits"
+    }
+</script>
+
+<style scoped>
+
+</style>
