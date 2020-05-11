c40538251b8984f36a54f374a04d971211b2e1d2
Remy <wisteetee@gmail.com>
2020-05-11 09:24:31 +0200

    ajout ressources(images)/Accueil

--- HDV_Online_FrontV2/pages/commandes.vue
+++ HDV_Online_FrontV2/pages/commandes.vue
@@ -1,13 +1,35 @@
 <template>
-
+  <v-container fluid>
+    <v-row>
+      
+    </v-row>
+  </v-container>
 </template>
 
 <script>
     export default {
-        name: "commandes"
+        name: "commandes",
+
+      data () {
+        return {
+          alignmentsAvailable: [
+            'start',
+            'center',
+            'end',
+            'baseline',
+            'stretch',
+          ],
+          alignment: 'center',
+          dense: false,
+          justifyAvailable: [
+            'start',
+            'center',
+            'end',
+            'space-around',
+            'space-between',
+          ],
+          justify: 'center',
+        }
+      },
     }
 </script>
-
-<style scoped>
-
-</style>
