430e18e6655b109a2f7dffc3f31ec66512850a45
Remy <wisteetee@gmail.com>
2020-05-13 01:49:31 +0200

    tabprod

--- HDV_Online_FrontV2/pages/commandes.vue
+++ HDV_Online_FrontV2/pages/commandes.vue
@@ -5,9 +5,8 @@
 
 
     <v-data-table
-      :headers="headers"
+      :headers="headersCommande"
       :items="ListeCommande"
-      :search="search"
       class="elevation-1 pt-6 mt-6"
     >
       <template v-slot:top>
@@ -20,8 +19,8 @@
       <template v-slot:body="{ items }">
         <tbody>
         <tr v-for="item in ListeCommande" @click="displayItem(item)">
-          <td>{{item.nomPays}} </td>
-          <td>{{item.client}}</td>
+          <td>{{item.nomProduit}} </td>
+          <td>{{item.prix}}</td>
           <td>{{item.date}}</td>
           <td>{{item.montant}}</td>
           <td>{{item.etat}}</td>
@@ -29,6 +28,34 @@
         </tbody>
       </template>
     </v-data-table>
+
+
+    <v-data-table
+      :headers="headersProduits"
+      :items="ListeProduit"
+      class="elevation-1 pt-6 mt-6"
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>liste des produits</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+      </template>
+
+      <template v-slot:body="{ items }">
+        <tbody>
+          <tr v-for="item in ListeProduit" @click="displayItem(item)">
+            <td>{{item.nomProduit}} </td>
+            <td>{{item.prix}} €</td>
+            <td>{{item.quantiteProd}}</td>
+            <td>{{item.descriptionProduit}}</td>
+            <td><v-model ><v-btn v-on:click="decrement" rounded small><v-icon>mdi-minus</v-icon></v-btn> {{ ListeProduit.indexOf(item) }} <v-btn rounded small v-on:click="increment"><v-icon>mdi-plus</v-icon></v-btn></v-model></td>
+          </tr>
+        </tbody>
+      </template>
+    </v-data-table>
+
+
   </v-container>
 </template>
 
@@ -58,7 +85,7 @@
 
           expanded: [],
           singleExpand: false,
-          headers: [
+          headersCommande: [
             {
               text: 'Identifiant de commande',
               align: 'start',
@@ -70,8 +97,27 @@
             { text: 'Montant', value: 'montant' },
             { text: 'Etat', value: 'etat' },
           ],
+
+          headersProduits: [
+            {
+              text: 'liste des produits',
+              align: 'start',
+              sortable: false,
+              value: 'nomProduit',
+            },
+            { text: 'tarif', value: 'prix' },
+            { text: 'stock', value: 'montant' },
+            { text: 'description produit', value: 'etat' },
+            { text: 'ajout', value: 'etat' },
+          ],
           ListeCommande: [],
+          ListeProduit: [],
           CommandeId:null,
+          BtnValue:[
+            {index: 0, value: 0},
+            {index: 1, value: 1},
+            {index: 2, value: 2}
+          ],
         }
       },
       methods:{
@@ -79,10 +125,26 @@
           console.log(item),
             this.CommandeId = this.ListeCommande.indexOf(item)
         },
+        addItemInShoppingList(){
+
+        },
+        increment() {
+          this.BtnValue++;
+
+        },
+        decrement() {
+          if(this.BtnValue>=1)
+          this.BtnValue--;
+        }
       },
       mounted() {
-        this.$axios.$get('https://localhost:5001/api/Pays')
+        this.$axios.$get('https://localhost:44391/api/commandes')
           .then(response => this.ListeCommande=response)
+          .then(response => console.log(response))
+          .catch(error => console.log(error))
+
+        this.$axios.$get('https://localhost:44391/api/produits')
+          .then(response => this.ListeProduit=response)
           .then(response => console.log(response))
           .catch(error => console.log(error))
       },
