d9732b7d7e0c51ad09e390a8be3b63d4318fe1ae
Remy <wisteetee@gmail.com>
2020-05-13 22:10:44 +0200

    ajout gestion commandes + remake Accueil

--- HDV_Online_FrontV2/pages/Accueil.vue
+++ HDV_Online_FrontV2/pages/Accueil.vue
@@ -1,111 +1,134 @@
 <template>
-    <v-container class="pr-6">
-      <v-carousel :show-arrows="false">
-        <v-carousel-item
-          v-for="(item,i) in itemsCarousel"
-          :key="i"
-          :src="require('@/assets/ressources/' + item.src)"
-        ></v-carousel-item>
-      </v-carousel>
-      <v-container fluid>
-        <tbody>
-        <v-system-bar style="width: 100%">Top Fournitures</v-system-bar>
+  <v-container class="pr-6">
+    <v-carousel :show-arrows="true">
+      <v-carousel-item
+        v-for="(item,i) in itemsCarousel"
+        :key="i"
+        :src="require('@/assets/ressources/' + item.src)"
+      ></v-carousel-item>
+    </v-carousel>
+    <v-container fluid style="width: 100%">
+
+      <v-system-bar style="width: 100%">Top Fournitures</v-system-bar>
 
 
-          <v-layout row>
-            <v-flex md3 v-for="(produit) in itemsVente" :key="produit.title">
-              <v-card
-                style="height: 350px;"
-                class="ml-5 mx-auto my-8"
-                max-width="400"
+      <v-layout row>
+        <v-flex md3 v-for="(produit) in itemsVente" :key="produit.title">
+          <v-card
+            style="height: 350px;"
+            class="ml-5 mx-auto my-8"
+            max-width="400"
 
-              >
-                <v-img
-                  height="200px"
-                  contain
-                  :src="require('@/assets/ressources/' + produit.src)"
-                ></v-img>
+          >
+            <v-img
+              height="200px"
+              contain
+              :src="require('@/assets/ressources/' + produit.src)"
+            ></v-img>
 
-                <v-card-title>
-                  {{ produit.titre }}
-                </v-card-title>
+            <v-card-title>
+              {{ produit.titre }}
+            </v-card-title>
 
-                <v-card-subtitle>
-                  {{ produit.description}}
-                </v-card-subtitle>
-                <v-card-text>PRIX: {{produit.prix}}€</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
+            <v-card-subtitle>
+              {{ produit.description}}
+            </v-card-subtitle>
+            <v-card-text>PRIX: {{produit.prix}}€</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
 
-        <v-system-bar style="width: 100%">Produits à bas prix</v-system-bar>
-
-
-          <v-layout row class="my-5">
-            <v-flex md4 v-for="(produit) in itemsVentePromo" :key="produit.title">
-              <v-card
-                style="height: 350px;"
-                class="ml-5 mt-3 mx-auto"
-                max-width="500"
-              >
-                <v-img
-                  height="200px"
-                  contain
-                  :src="require('@/assets/ressources/' + produit.src)"
-                >
-
-                </v-img>
-
-                <v-card-title>
-                  {{ produit.titre }}
-                </v-card-title>
-
-                <v-card-subtitle>
-                  {{ produit.description}}
-                </v-card-subtitle>
-                <v-card-text>PRIX: {{produit.prix}}€</v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
+      <v-system-bar style="width: 100%">Produits à bas prix</v-system-bar>
 
 
-        <v-row style="height: 1000px;">
-
-        </v-row>
-
-        </tbody>
-      </v-container>
-      <v-footer
-        dark
-        padless
-      >
-        <v-card
-          flat
-          tile
-          class="orange lighten-1 white--text text-center"
-        >
-          <v-card-text>
-            <v-btn
-              v-for="icon in icons"
-              :key="icon"
-              class="mx-4 white--text"
-              icon
+      <v-layout row class="my-5">
+        <v-flex md4 v-for="(produit) in itemsVentePromo" :key="produit.title">
+          <v-card
+            style="height: 350px;"
+            class="ml-5 mt-8 mx-auto"
+            max-width="500"
+          >
+            <v-img
+              height="200px"
+              contain
+              :src="require('@/assets/ressources/' + produit.src)"
             >
-              <v-icon size="24px">{{ icon }}</v-icon>
-            </v-btn>
-          </v-card-text>
 
-          <v-card-text class="white--text pt-0">
-            © 2020 CesiCorp. Tous droits réservés.En vous inscrivant sur DOFUS, vous vous engagez à respecter un certain nombre de règles. Celles-ci sont là pour maintenir une ambiance conviviale et optimiser le confort de jeu de chaque joueur. Dans le cas où un joueur choisit de contrevenir à ces règles, il doit savoir qu'il s'expose à des sanctions.
-          </v-card-text>
+            </v-img>
 
-          <v-divider></v-divider>
+            <v-card-title>
+              {{ produit.titre }}
+            </v-card-title>
 
-          <v-card-text class="white--text">
-            {{ new Date().getFullYear() }} — <strong>DofusSHOP</strong>
-          </v-card-text>
-        </v-card>
-      </v-footer>
+            <v-card-subtitle>
+              {{ produit.description}}
+            </v-card-subtitle>
+            <v-card-text>PRIX: {{produit.prix}}€</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+      <v-system-bar style="width: 100%">Maxi Pack</v-system-bar>
+
+      <v-layout row>
+        <v-flex md4 v-for="(produit) in itemsVentePack" :key="produit.title">
+          <v-card
+            style="height: 350px;"
+            class="ml-5 mx-auto my-8"
+            max-width="600"
+
+          >
+            <v-img
+              height="200px"
+              contain
+              :src="require('@/assets/ressources/' + produit.src)"
+            ></v-img>
+
+            <v-card-title>
+              {{ produit.titre }}
+            </v-card-title>
+
+            <v-card-subtitle>
+              {{ produit.description}}
+            </v-card-subtitle>
+            <v-card-text>PRIX: {{produit.prix}}€</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+
+
+  
+    </v-container>
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="orange lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text class="white--text pt-0">
+          © 2020 CesiCorp. Tous droits réservés.En vous inscrivant sur DOFUS, vous vous engagez à respecter un certain nombre de règles. Celles-ci sont là pour maintenir une ambiance conviviale et optimiser le confort de jeu de chaque joueur. Dans le cas où un joueur choisit de contrevenir à ces règles, il doit savoir qu'il s'expose à des sanctions.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>DofusSHOP</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-container>
 </template>
 
@@ -124,7 +147,6 @@
       ],
       itemsCarousel: [
         {
-
           src: 'wallpaper/carrousel1.jpg',
         },
         {
@@ -139,16 +161,23 @@
       ],
       itemsVente: [
         {titre:'Top vente de bois Dofus', description:'1000 unités de bois', prix:'10',src:'bucherons/chataigner.png'},
-        {titre:'Top vente de minerais Dofus', description:'1000 unités de minerais', prix:'15',src:'mineur/ARGENT.png'},
-        {titre:'Top vente de fer Dofus', description:'1000 unités de fer', prix:'20',src:'mineur/FER.png'},
-        {titre:'Top vente d\'or Dofus', description:"1000 unités d\'or", prix:'25',src:'mineur/OR.png'},
-        ],
-      itemsVentePromo: [
-        {titre:'Pack bois + ressources', description:'1000 unités de bois + 1000 unités de minerais + 1000 kamas', prix:'50',src:'pack_bois_1.png'},
-        {titre:'Top vente de bois Dofus', description:'100 unités de bois', prix:'10',src:'bucherons/chene.png'},
-        {titre:'Top vente de bois Dofus', description:'100 unités de fer', prix:'20',src:'bucherons/noyer.png'},
+        {titre:'Top vente de minerais Dofus', description:'1000 unités de minerais', prix:'15',src:'mineur/argent.png'},
+        {titre:'Top vente de fer Dofus', description:'1000 unités de fer', prix:'20',src:'mineur/fer.png'},
+        {titre:'Top vente d\'or Dofus', description:"1000 unités d\'or", prix:'25',src:'mineur/or.png'},
       ],
-
+      itemsVentePromo: [
+        {titre:'Top vente de minerais Dofus', description:'1000 unités de minerais + 1000 unités de bois + 1000 kamas', prix:'50',src:'mineur/bauxite.png'},
+        {titre:'Top vente de bois Dofus', description:'100 unités de bois', prix:'5',src:'bucherons/chene.png'},
+        {titre:'Top vente de bois Dofus', description:'100 unités de fer', prix:'10',src:'bucherons/noyer.png'},
+        {titre:'Top vente de bois de Charmes', description:'100 unités de bois', prix:'10',src:'bucherons/charme.png'},
+        {titre:'Top vente de minerais Dofus', description:'100 unités d\' olivier', prix:'20',src:'bucherons/olivier.png'},
+        {titre:'Top vente de silicate ', description:'1000 unités de silicate', prix:'20',src:'mineur/silicate.png'},
+      ],
+      itemsVentePack: [
+        {titre:'Pack bois + ressources', description:'1000 unités de bois + 1000 unités de minerais + 1000 kamas', prix:'50',src:'pack_bois_1.png'},
+        {titre:'Top vente de bois Dofus', description:'100 unités de bois', prix:'100',src:'pack_3.png'},
+        {titre:'Pack bois + minerais + arme légendaire', description:'100 unités de fer, 100 bois, 1 arme légendaire', prix:'20',src:'pack_2.png'},
+      ],
       show: false,
     }),
   }
--- HDV_Online_FrontV2/pages/commandes.vue
+++ HDV_Online_FrontV2/pages/commandes.vue
@@ -1,60 +1,54 @@
 <template>
   <v-container fluid>
 
+    <v-form ref="form">
+        <v-select
+          placeholder="Ajoutez vos produits"
+          :items="Produits"
+          item-text="nomProduit"
+          item-value="id"
+          v-model="ListeProduit.ProduitId"
+        >
+        </v-select>
+        <v-text-field
+          type="number"
+          placeholder="add quantité"
+          v-model="ListeProduit.QuantiteCommande"
+        ></v-text-field>
+        <v-btn
+        @click="ajouter">Ajouter</v-btn>
 
+        <v-system-bar style="width: 100%" class="mt-5"></v-system-bar>
 
+        <v-data-table
+          :headers="headersCommande"
+          :items="Commande"
+          class="elevation-1 pt-6 mt-6"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Mes commandes</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+          </template>
 
-    <v-data-table
-      :headers="headersCommande"
-      :items="ListeCommande"
-      class="elevation-1 pt-6 mt-6"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>Mes commandes</v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-toolbar>
-      </template>
-
-      <template v-slot:body="{ items }">
-        <tbody>
-        <tr v-for="item in ListeCommande" @click="displayItem(item)">
-          <td>{{item.nomProduit}} </td>
-          <td>{{item.prix}}</td>
-          <td>{{item.date}}</td>
-          <td>{{item.montant}}</td>
-          <td>{{item.etat}}</td>
-        </tr>
-        </tbody>
-      </template>
-    </v-data-table>
-
-
-    <v-data-table
-      :headers="headersProduits"
-      :items="ListeProduit"
-      class="elevation-1 pt-6 mt-6"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title>liste des produits</v-toolbar-title>
-          <v-spacer></v-spacer>
-        </v-toolbar>
-      </template>
-
-      <template v-slot:body="{ items }">
-        <tbody>
-          <tr v-for="item in ListeProduit" @click="displayItem(item)">
-            <td>{{item.nomProduit}} </td>
-            <td>{{item.prix}} €</td>
-            <td>{{item.quantiteProd}}</td>
-            <td>{{item.descriptionProduit}}</td>
-            <td><v-model ><v-btn v-on:click="decrement" rounded small><v-icon>mdi-minus</v-icon></v-btn> {{ BtnValue[ListeProduit.indexOf(item)] }} <v-btn rounded small v-on:click="increment"><v-icon>mdi-plus</v-icon></v-btn></v-model></td>
-          </tr>
-        </tbody>
-      </template>
-    </v-data-table>
-
+          <template v-slot:body="{ items }">
+            <tbody>
+              <tr v-for="item in items">
+                <td >{{ item.IdProduit }} </td>
+                <td>{{ item.IdProduit }}</td>
+                <td>{{ item.QuantiteProduit }}</td>
+                <td>{{ item.QuantiteProduit }}</td>
+              </tr>
+              <span v-if="Commande.length<=0">Panier vide</span>
+              <span v-if="Commande.length!=0">Nombre d'articles: {{ Commande.length }}</span>
+            </tbody>
+          </template>
+        </v-data-table>
+<!--      Produits:{{ Produits }}
+      ListeProduit{{ ListeProduit}}
+      Commande{{ Commande}}-->
+    </v-form>
 
   </v-container>
 </template>
@@ -87,65 +81,45 @@
           singleExpand: false,
           headersCommande: [
             {
-              text: 'Identifiant de commande',
+              text: 'Produit',
               align: 'start',
               sortable: false,
               value: 'name',
             },
-            { text: 'Client', value: 'client' },
-            { text: 'Date', value: 'date' },
+            { text: 'Id produit', value: 'client' },
+            { text: 'Quantité', value: 'date' },
             { text: 'Montant', value: 'montant' },
-            { text: 'Etat', value: 'etat' },
           ],
 
-          headersProduits: [
-            {
-              text: 'liste des produits',
-              align: 'start',
-              sortable: false,
-              value: 'nomProduit',
-            },
-            { text: 'tarif', value: 'prix' },
-            { text: 'stock', value: 'montant' },
-            { text: 'description produit', value: 'etat' },
-            { text: 'ajout', value: 'etat' },
-          ],
-          ListeCommande: [],
-          ListeProduit: [],
-          CommandeId:null,
-          BtnValue:[
-            {value:0},
-            {value: 1},
-            {value: 2}
-          ],
+
+          Produits: [],
+          ListeProduit:{
+            IdLigne:'',
+            ProduitId:'',
+            QuantiteCommande:'',
+            ProduitNom:'',
+          },
+          Commande:[],
+          lastCommande:'',
         }
       },
       methods:{
-        displayItem (item) {
-          console.log(item),
-            this.CommandeId = this.ListeCommande.indexOf(item)
+        ajouter(){
+          this.Commande.push({
+          IdProduit:this.ListeProduit.ProduitId,
+          QuantiteProduit:this.ListeProduit.QuantiteCommande,
+          IdCommande:this.lastCommande
+          })
+         /* this.$refs.form.reset()*/
         },
-        addItemInShoppingList(){
-
-        },
-        increment() {
-
-          this.BtnValue++;
-
-        },
-        decrement() {
-          if(this.BtnValue>=1)
-          this.BtnValue--;
-        }
       },
       mounted() {
-        this.$axios.$get('https://localhost:44391/api/commandes')
-          .then(response => this.ListeCommande=response)
+        this.$axios.$get('/produits')
+          .then(response => this.Produits=response)
           .then(response => console.log(response))
           .catch(error => console.log(error))
-
-        this.$axios.$get('https://localhost:44391/api/produits')
-          .then(response => this.ListeProduit=response)
+        this.$axios.$get('/commandes/last')
+          .then(response => this.lastCommande=response)
           .then(response => console.log(response))
           .catch(error => console.log(error))
       },
